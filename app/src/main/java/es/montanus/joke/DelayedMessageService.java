db81905a2a7e3c436dfadda91cd1102b45ab5fd2
Noel Díaz Rodríguez <noeldiazro@gmail.com>
2018-05-30 11:07:22 +0200

    Message displayed as notification

--- app/src/main/java/es/montanus/joke/DelayedMessageService.java
+++ app/src/main/java/es/montanus/joke/DelayedMessageService.java
@@ -1,8 +1,12 @@
 package es.montanus.joke;
 
 import android.app.IntentService;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Intent;
-import android.util.Log;
+import android.support.annotation.NonNull;
+import android.support.v4.app.NotificationCompat;
 
 import org.jetbrains.annotations.Nullable;
 
@@ -18,9 +22,8 @@ public class DelayedMessageService extends IntentService {
     @Override
     protected void onHandleIntent(@Nullable Intent intent) {
         tryWaiting(10000);
-        if (intent != null) {
-            Log.v(SERVICE_NAME, intent.getStringExtra(EXTRA_MESSAGE));
-        }
+
+        if (intent != null) issue(getNotification(intent));
     }
 
     private void tryWaiting(long millis) {
@@ -31,5 +34,29 @@ public class DelayedMessageService extends IntentService {
                 e.printStackTrace();
             }
         }
+    }
+
+    private void issue(Notification notification) {
+        NotificationManager notificationManager =
+                (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        notificationManager.notify(5453, notification);
+    }
+
+    private Notification getNotification(@NonNull Intent intent) {
+        return new NotificationCompat.Builder(this)
+                .setSmallIcon(android.R.drawable.sym_def_app_icon)
+                .setContentTitle(getString(R.string.question))
+                .setContentText(intent.getStringExtra(EXTRA_MESSAGE))
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
+                .setVibrate(new long[]{0, 1000})
+                .setAutoCancel(true)
+                .setContentIntent(createAction())
+                .build();
+    }
+
+    private PendingIntent createAction() {
+        final Intent actionIntent = new Intent(this, MainActivity.class);
+        return PendingIntent.getActivity(
+                this, 0, actionIntent, PendingIntent.FLAG_UPDATE_CURRENT);
     }
 }
